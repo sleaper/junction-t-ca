55e40e3b0479dd65cf68785e06aaa9455532b777
Petr Spac <petrik.spac@gmail.com>
2025-10-24 23:08:10 +0200

    use classes, drawing

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 
 #include <array>
 #include <cstdint>
+#include <memory>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
 #include "CImg.h"
@@ -15,104 +18,247 @@ const uint8_t MIDDLE_ROAD[] = {150, 150, 150};
 const uint8_t SOUTH_ROAD[] = {200, 200, 200};
 const uint8_t WEST_ROAD[] = {100, 100, 100};
 const uint8_t EAST_ROAD[] = {0, 255, 0};
+const uint8_t INTERSECTION_COLOR[] = {255, 0, 0};
 
-const int CELLS_X = 100;  // 750 / 100 = 7.5m per cell
+const int MAIN_LANE_LENGTH = 100;  // in cells
+const int MINOR_LANE_LENGTH = 18;
+const int SLIP_LANE_LENGTH = 17;
+
+const int SCREEN_CELLS_X = 100;  // 750 / 100 = 7.5m per cell
+const int SCREEN_CELLS_Y = 100;
+
 constexpr double CELL_LENGTH_M = 7.5;
-
-const int CELLS_Y = 100;
-const int CELL_SIZE = 10;
+const int CELL_SIZE = 10;  // in pixels
 
 const int DELTA = 1;  // 1 second of model time per step
 const int VMAX = 9;
 
-const double FLOW_RATE = 0.684;    // cars per second (2464 cars/hour)
+const double FLOW_RATE_MAJOR = 0.684;
+const double FLOW_RATE_MINOR = 0.2;
+
 const int CAR_SPAWN_DISTANCE = 1;  // Minimum cells gap before spawning
 
-typedef struct Car {
+enum class Direction { WEST, EAST, NORTH, SOUTH };
+enum class LaneType { THROUGH, TURN, MIXED };
+
+struct Lane;
+struct Car;
+class IntersectionManager;
+
+struct Car {
     int speed = 0;
-    size_t x = 0;
-} Car;
+    size_t pos = 0;
+    Direction direction;
+    float aggression = 0.0;
+    Lane* lane = nullptr;
+    bool wants_to_turn = false;
+    size_t id;
 
-std::vector<Car> cars;
-
-int distance_to_next_car(size_t car_index) {
-    if (car_index >= cars.size() - 1) {  // last car
-        // No car ahead
-        return -1;
-    } else {
-        return (int)(cars.at(car_index + 1).x - cars.at(car_index).x - 1);
+    Car(Direction dest, float aggr, Lane* lane, size_t id)
+        : direction(dest), aggression(aggr), lane(lane), id(id) {
+        speed = static_cast<int>(rand() % VMAX) + 1;
     }
+
+    void draw(CImg<unsigned char>& img) const {
+        auto [x, y] = lane->get_position(pos);
+        img.draw_point(x, y, CAR_COLOR, 1);
+    }
+};
+
+struct Lane {
+    Direction dir;
+    LaneType type;
+    std::vector<Car*> cars;
+    int len_cels;
+    std::string id;
+    int start_x, start_y;
+    bool is_vertical;
+
+    Lane(Direction dir, LaneType type, int len_cels, std::string id,
+         int start_x, int start_y, bool is_vertical)
+        : dir(dir),
+          type(type),
+          len_cels(len_cels),
+          id(id),
+          start_x(start_x),
+          start_y(start_y),
+          is_vertical(is_vertical) {}
+
+    std::pair<int, int> get_direction_vector() const {
+        switch (dir) {
+            case Direction::WEST:
+                return {-1, 0};
+            case Direction::EAST:
+                return {1, 0};
+            case Direction::NORTH:
+                return {0, -1};
+            case Direction::SOUTH:
+                return {0, 1};
+            default:
+                throw std::invalid_argument("Invalid direction");
+        }
+    }
+
+    /**
+     * @brief Get the position of the cell on the lane
+     * @param pos The position of the cell on the lane, 0 means the start of the
+     * line, len_cels means the end of the line
+     * @return The position of the cell in pixels
+     */
+    std::pair<int, int> get_position(size_t pos) const {
+        auto [dx, dy] = get_direction_vector();
+        return {start_x + dx * static_cast<int>(pos),
+                start_y + dy * static_cast<int>(pos)};
+    }
+
+    inline const uint8_t* get_road_color() const {
+        switch (dir) {
+            case Direction::WEST:
+                return WEST_ROAD;
+            case Direction::NORTH:
+                return NORTH_ROAD;
+            case Direction::EAST:
+                return EAST_ROAD;
+            case Direction::SOUTH:
+                return SOUTH_ROAD;
+            default:
+                throw std::invalid_argument("Invalid direction");
+        }
+    }
+
+    int distance_to_next_car(size_t car_index) const {
+        if (car_index >= cars.size() - 1) {
+            return -1;
+        } else {
+            return static_cast<int>(cars.at(car_index + 1)->pos -
+                                    cars.at(car_index)->pos - 1);
+        }
+    }
+
+    Car* find_car_at_pos(size_t pos) const {
+        for (auto& car : cars) {
+            if (car->pos == pos) return car;
+        }
+        return nullptr;
+    }
+
+    void draw(CImg<unsigned char>& img) const {
+        auto [x1, y1] = get_position(0);
+        auto [x2, y2] = get_position(len_cels);
+        img.draw_line(x1, y1, x2, y2, get_road_color(), 1);
+    }
+};
+
+class IntersectionManager {
+   public:
+    std::vector<Lane*> lanes;
+
+    IntersectionManager() {}
+
+    void add_lane(Lane* lane) { lanes.push_back(lane); }
+
+   private:
+};
+
+// Global state
+std::vector<std::unique_ptr<Lane>> g_lanes;
+std::vector<std::unique_ptr<Car>> g_cars;
+IntersectionManager intersection_manager;
+size_t next_car_id = 0;
+
+void add_lane_to_system(std::unique_ptr<Lane> lane) {
+    intersection_manager.add_lane(lane.get());
+    g_lanes.push_back(std::move(lane));
+}
+
+void init_lanes() {
+    int y_mid = SCREEN_CELLS_Y / 2;
+    int x_mid = SCREEN_CELLS_X / 2;
+    int y_last = SCREEN_CELLS_Y - 1;
+    int x_last = SCREEN_CELLS_X - 1;
+    int major_lane_offset = 3;
+    int slip_lane_offset = 1;
+
+    // Major lanes
+    auto west =
+        std::make_unique<Lane>(Direction::WEST, LaneType::THROUGH,
+                               MAIN_LANE_LENGTH, "west", x_last, y_mid, false);
+
+    auto slip = std::make_unique<Lane>(
+        Direction::WEST, LaneType::TURN, SLIP_LANE_LENGTH, "slip",
+        x_mid + SLIP_LANE_LENGTH + 1, y_mid + slip_lane_offset, false);
+
+    auto east =
+        std::make_unique<Lane>(Direction::EAST, LaneType::THROUGH,
+                               MAIN_LANE_LENGTH, "east", 0, y_mid + 2, false);
+
+    auto north = std::make_unique<Lane>(
+        Direction::NORTH, LaneType::THROUGH, MINOR_LANE_LENGTH, "north", x_mid,
+        y_mid + MINOR_LANE_LENGTH + major_lane_offset, true);
+
+    auto south = std::make_unique<Lane>(Direction::SOUTH, LaneType::THROUGH,
+                                        MINOR_LANE_LENGTH, "south", x_mid - 1,
+                                        y_mid + major_lane_offset, true);
+
+    add_lane_to_system(std::move(west));
+    add_lane_to_system(std::move(slip));
+    add_lane_to_system(std::move(east));
+    add_lane_to_system(std::move(north));
+    add_lane_to_system(std::move(south));
 }
 
 void draw(CImg<unsigned char>& img) {
     img.fill(0);
-    int y_mid = img.height() / 2;
-    int y_last = img.height() - 1;
-    int x_last = img.width() - 1;
 
-    img.draw_line(0, y_mid, x_last, y_mid, NORTH_ROAD, 1);
-    img.draw_line(0, y_mid + 1, x_last, y_mid + 1, MIDDLE_ROAD, 1);
-    img.draw_line(0, y_mid + 2, x_last, y_mid + 2, SOUTH_ROAD, 1);
-    img.draw_line(x_last / 2, y_mid, x_last / 2, y_last, WEST_ROAD, 1);
-    img.draw_line((x_last / 2) - 1, y_mid, (x_last / 2) - 1, y_last, EAST_ROAD,
-                  1);
+    for (auto& lane : g_lanes) {
+        lane->draw(img);
+    }
 
-    for (auto& car : cars) {
-        if (car.x >= CELLS_X) continue;  // Out of bounds
-        img.draw_point((int)car.x, y_mid, CAR_COLOR, 1);
+    // Draw intersection TODO: Maybe put into IntersectionManager ??
+    for (int dx = 0; dx < 2; dx++) {
+        for (int dy = 0; dy < 3; dy++) {
+            int x = SCREEN_CELLS_X / 2 + dx - 1;
+            int y = SCREEN_CELLS_Y / 2 + dy;
+            img.draw_point(x, y, INTERSECTION_COLOR, 1);
+        }
+    }
+
+    for (auto& car : g_cars) {
+        car->draw(img);
     }
 }
 
-void sim_step() {
-    if ((cars.empty() || cars.front().x != 0) &&
-        (double)rand() / RAND_MAX < FLOW_RATE) {
-        cars.insert(cars.begin(), {(rand() % VMAX) + 1, 0});
-    }
+// void sim_step() {
+//     spawn_cars();
 
-    // R1: Acceleration
-    for (auto& car : cars) {
-        if (car.speed < VMAX) car.speed += 1;
-    }
+//     for (auto& car : g_cars) {
+//         attempt_lane_change(car.get());
+//     }
 
-    // R2: Deceleration
-    for (size_t i = 0; i < cars.size(); i++) {
-        int distance = distance_to_next_car(i);
-        if (distance == -1) continue;  // No car ahead
+//     for (auto& lane : g_lanes) {
+//         apply_nash_rules(lane.get());
+//     }
 
-        cars[i].speed = std::min(cars[i].speed, distance - 1);
-    }
-
-    // R3: Randomization TODO: Pick right probability
-    for (auto& car : cars) {
-        if (rand() % 2 == 0) car.speed = std::max(0, car.speed - 1);
-    }
-
-    // R4: Car movement
-    for (auto& car : cars) {
-        car.x += car.speed;
-    }
-
-    // Remove cars that are out of bounds
-    cars.erase(std::remove_if(cars.begin(), cars.end(),
-                              [](const Car& car) { return car.x >= CELLS_X; }),
-               cars.end());
-}
+//     remove_out_of_bounds_cars();
+// }
 
 int main() {
+    init_lanes();
+
     srand(static_cast<unsigned int>(time(nullptr)));
 
     bool visualize = true;
 
-    const int WIN_W = CELLS_X * CELL_SIZE;
-    const int WIN_H = CELLS_Y * CELL_SIZE;
-    CImg<unsigned char> grid(CELLS_X, CELLS_Y, 1, 3, 0);
+    const int WIN_W = SCREEN_CELLS_X * CELL_SIZE;
+    const int WIN_H = SCREEN_CELLS_Y * CELL_SIZE;
+    CImg<unsigned char> grid(SCREEN_CELLS_X, SCREEN_CELLS_Y, 1, 3, 0);
 
     CImgDisplay win;
 
     if (visualize) win = CImgDisplay(WIN_W, WIN_H, "Simulation grid");
 
     for (unsigned long mt = 0; mt < 10000; mt += DELTA) {
-        sim_step();
+        // sim_step();
 
         if (visualize) {
             draw(grid);
